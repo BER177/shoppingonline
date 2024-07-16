d09c460a2470f07877e35fbd28e9476a0bb72def
BER177 <145816634+BER177@users.noreply.github.com>
2024-07-16 10:42:41 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster1.rkzqbw6.mongodb.net',
+  DB_USER: 'hoangtri',
+  DB_PASS: '1772002',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'caigicungduoc',
+  JWT_EXPIRES: '3600000', // in milliseconds
+  EMAIL_USER: 'hoangtri1772002', // gmail service
+  EMAIL_PASS: 'grpf uwpz egok jprv'
+};
+module.exports = MyConstants;
